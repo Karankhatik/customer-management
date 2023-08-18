01582c4a78bcc4f88679dc844a56af4fbe96b3d9
Karan Khatik <karan13699@gmail.com>
2023-08-18 10:26:47 +0530

    Fixed

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Not Found!</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Not Found!!</h1>
       <p className="text-lg text-gray-600">
         Oops! The page you're looking for could not be found.
       </p>
